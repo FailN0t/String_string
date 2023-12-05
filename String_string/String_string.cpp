6933f02ab3c30ee3d597c8d4fe5e8f9766b4eeaf
Igor Putyajkin <putyajkin99@gmail.com>
2023-12-05 18:53:31 +0300

    init

--- String_string/String_string.cpp
+++ String_string/String_string.cpp
@@ -2,10 +2,71 @@
 //
 
 #include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string str{ "abcdefgfdgsfde" };
+    cout << str[0] << endl;
+    cout << str.at(2) << endl;
+    cout << str.size() << endl;
+    cout << str.front() << endl;
+    cout << *(str.begin()) << endl;
+    for (auto it = str.begin(); it < str.end(); it++) {
+        cout << *it;
+    }
+    cout << endl;
+    cout << str.find("cde") << endl;
+    cout << str.rfind("cde") << endl;
+    cout << str.find_first_of("cde") << endl;
+    cout << str.find_last_of("cde") << endl;
+    cout << str.find_last_not_of("cdefg") << endl;
+    str.assign("Hello");
+    cout << str << endl;
+    str.clear();
+    cout << str << endl;
+    str = "dasdvasvdasdv";
+    cout << str << endl;
+    str.push_back('H');
+    cout << str << endl;
+    str.pop_back();
+    cout << str << endl;
+    str.append("Helllo");
+    cout << str << endl;
+    str += "Hello";
+    cout << str << endl;
+
+
+
+    cout << str.empty() << endl;
+    cout << str.max_size() << endl;
+    cout << str.capacity() << endl;
+
+
+    string str2{ "123.1" };
+    int n = stoi(str2);
+    long n2 = stol(str2);
+    long long n3 = stoll(str2);
+
+    float f = stof(str2);
+    double d = stod(str2);
+    cout << n << endl;
+    cout << n2 << endl;
+    cout << n3 << endl;
+
+    cout << f << endl;
+    cout << d << endl;
+
+    str = to_string(45);
+    cout << str << endl;
+
+    cout << str << "  " << str2 << endl;
+    swap(str, str2);
+    cout << str << "  " << str2 << endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
